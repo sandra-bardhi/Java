6bf93db2f782e95a2d6f6632c79ed6594b62633f
sandra-bardhi <bardhisandra@gmail.com>
2022-06-04 17:27:22 +0200

    javaFirstProgram

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hi my name is Sandra Bardhi!");
+        System.out.println("I am 22 years old!");
+        System.out.println("My hometown is Elbasan,Albania.");
+    }
+}
